a3f7d9f3cdb5328fc37222046fc8a9957de7c75e
valcomp <valerie.aleida.lopez@gmail.com>
2022-05-21 15:42:07 -0500

    reset method

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,11 +7,19 @@ var taskFormHandler = function(event){
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    //check if input values are empty strings
+    if(!taskNameInput || !taskTypeInput){
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
+
+    formEl.reset();
 
     createTaskEl(taskDataObj);
 
